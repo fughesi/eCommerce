1be93ee2ee0e6b6d26dd38da4dd44b9d7c14a9d6
Steven <steven.camillieri@protonmail.com>
2022-07-17 16:31:23 -0400

    added graphics files

---
+++ src/JSON/_cartoonJSON.tsx
@@ -0,0 +1,42 @@
+import { nanoid } from "nanoid";
+import Cat from "../resources/buttons/CartoonCats.png";
+import Dragon from "./../resources/buttons/cartoonDragons.png";
+import Monster from "./../resources/buttons/CartoonMonster.png";
+import Dog from "./../resources/buttons/cartoonDogs.png";
+
+const inventory: {}[] = [
+  {
+    id: nanoid(),
+    src: Cat,
+    alt: "Cartoon Cats",
+    link: "CatGraphics",
+    name: "Cartoon Cats",
+    quantity: 2,
+  },
+  {
+    id: nanoid(),
+    src: Dragon,
+    alt: "Dragon",
+    link: "DragonGraphics",
+    name: "Dragon",
+    quantity: 2,
+  },
+  {
+    id: nanoid(),
+    src: Monster,
+    alt: "Monster",
+    link: "MonsterGraphics",
+    name: "Monster",
+    quantity: 2,
+  },
+  {
+    id: nanoid(),
+    src: Dog,
+    alt: "Dogs",
+    link: "DogGraphics",
+    name: "Dogs",
+    quantity: 2,
+  },
+];
+
+export default inventory;
